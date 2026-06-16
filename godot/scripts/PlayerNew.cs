7a0e5a70d4bcc40f98b7e242bbe05e5cc9b669a3
CamilaM1010 <menendezcam443@gmail.com>
2026-06-16 16:18:02 +0900

    8-directional movement, AsStringName issue resolved

--- godot/scripts/PlayerNew.cs
+++ godot/scripts/PlayerNew.cs
@@ -1,4 +1,5 @@
 using Godot;
+using GodotStringIntercept;
 using System;
 
 public partial class PlayerNew : CharacterBody2D
@@ -8,80 +9,19 @@ public partial class PlayerNew : CharacterBody2D
 	{
 		this.Position = new Vector2(64, 64);
 	}
-
+	
 	[Export]
-	public float maxVelocity = 256;
-
-	[Export]
-	public float acceleration = 512;
-
-	[Export]
-	public float friction = 800;
+	public float speed = 50;
+	Vector2 direction = Vector2.Zero;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 direction = new Vector2(0, 0);
-		Vector2 acceleration_vector = new Vector2(0, 0);
-
-		if (Input.IsActionPressed("ui_right")) 
-		{
-			direction.X += 1;
-		}
-		if (Input.IsActionPressed("ui_left"))
-		{
-			direction.X -= 1;
-		}
-		if (Input.IsActionPressed("ui_down"))
-		{
-			direction.Y += 1;
-		}
-		if (Input.IsActionPressed("ui_up"))
-		{
-			direction.Y -= 1;
-		}
-
-
-		if (direction != Vector2.Zero) 
-		{
-			direction = direction.Normalized(); 
-			acceleration_vector = direction * acceleration; 
-		}
-		else 
-		{
-			acceleration_vector = Vector2.Zero; 
-		}
-
-		Velocity += acceleration_vector * (float)delta;
-		if (Velocity.Length() > maxVelocity) 
-		{
-			Velocity = Velocity.Normalized() * maxVelocity; 
-		}
-
 		MoveAndSlide();
-		//// Add the gravity.
-		//if (!IsOnFloor())
-		//{
-		//	velocity += GetGravity() * (float)delta;
-		//}
+		playerMovement();
+	}
 
-		//// Handle Jump.
-		//if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		//{
-		//	velocity.Y = JumpVelocity;
-		//}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		//Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		//if (direction != Vector2.Zero)
-		//{
-		//	velocity.X = direction.X * Speed;
-		//}
-		//else
-		//{
-		//	velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		//}
-
-		//Velocity = velocity;
+	public void playerMovement() {
+		direction = Input.GetVector("ui_left".AsStringName(), "ui_right".AsStringName(), "ui_up".AsStringName(), "ui_down".AsStringName());
+		Velocity = direction * speed;
 	}
 }
