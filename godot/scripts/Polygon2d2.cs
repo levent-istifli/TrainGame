5ecc23ff4235ba9c2c903526e151be8301ae0810
jas <jasage0@gmail.com>
2026-06-18 00:21:31 +0900

    added highlight feature/needs fixing

---
+++ godot/scripts/Polygon2d2.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Polygon2d2 : Polygon2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// prevent sprite from having polygon outline on initialization
+		Modulate = new Color("#0000");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public void ChangeColor() {
+		Modulate = new Color("#00FF0030");
+	}
+	
+	public void RevertColor() {
+		Modulate = new Color("#0000");
+	}
+}
