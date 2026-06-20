12153e421a5247e6c408976ba2824fb90240e605
planetmeep <100994529+planetmeep@users.noreply.github.com>
2026-06-20 17:54:31 +0900

    sample dialogue event

--- godot/scripts/DialogueLibrary.cs
+++ godot/scripts/DialogueLibrary.cs
@@ -8,7 +8,22 @@ public class DialogueLibrary
 
     public DialogueLibrary()
     {
-        
+        dialogueEvents["testDialogue"] = new DialogueEvent(async () =>
+        {
+            DialogueBoxUI ui = DialogueBoxUI.Instance;
+            await ui.DisplayText("Hello.");
+
+            ui.ChangeTextSpeed(7);
+
+            await ui.DisplayText("Here is a test question.");
+
+            /*int choice = await ui.DisplayChoice("Yes", "No");
+
+            if (choice == 0)
+                await d.Display("You picked yes.");
+            else
+                await d.Display("You picked no.");*/
+        });
     }
 
     public DialogueEvent GetEvent(string id)
