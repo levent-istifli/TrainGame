30c4dec04c76e8b8bb8c50470fdeb5519c691510
CamilaM1010 <menendezcam443@gmail.com>
2026-06-21 13:41:40 +0900

    Player is changed to be 1 player in main scene, not in every scene

--- godot/scripts/MainTestScene.cs
+++ godot/scripts/MainTestScene.cs
@@ -10,6 +10,8 @@ public partial class MainTestScene : Node2D
 		PackedScene startingScene = GD.Load<PackedScene>(scenePathToLoad);
 		Node startingSceneNode = startingScene.Instantiate();
 		AddChild(startingSceneNode);
+		CharacterBody2D player = GetNode<CharacterBody2D>("Player");
+		this.MoveChild(player, 1);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
