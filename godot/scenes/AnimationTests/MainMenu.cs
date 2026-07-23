98b2fa66ec7fa42cf195a4dc055e2d02bad8e5d4
CamilaM1010 <menendezcam443@gmail.com>
2026-07-24 01:30:48 +0900

    Moving Main menu

--- godot/scenes/AnimationTests/MainMenu.cs
+++ godot/scenes/AnimationTests/MainMenu.cs
@@ -6,15 +6,52 @@ public partial class MainMenu : Node2D
 	[Export] public PackedScene startScene;
 	[Export] public PackedScene creditScene;
 	[Export] public Node currentScene;
+	[Export] public Texture2D trainBackground;
+	[Export] public float bckgdSpeed = 100f;
+
+	private Sprite2D trainOne;
+	private Sprite2D trainTwo;
+	private float trainWidth = 1920;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+		trainOne = new Sprite2D
+		{
+			Texture = trainBackground,
+			Centered = false,
+			Position = new Vector2(0,0)
+		};
+        trainTwo = new Sprite2D
+        {
+            Texture = trainBackground,
+            Centered = false,
+            Position = new Vector2(trainWidth, 0)
+        };
 
+		AddChild(trainOne);
+		AddChild(trainTwo);
+
+    }
+
+    private enum MenuState { Moving, Stopping, Stopped, DoorsOpening, FadingOut }
+    private MenuState state = MenuState.Moving;
+    
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    public override void _Process(double delta)
 	{
+		if (state == MenuState.Stopping || state == MenuState.Moving) { 
+			float move = (float)(bckgdSpeed * delta);
+			trainOne.Position += new Vector2 (move, 0);
+			trainTwo.Position += new Vector2(move, 0);
+
+			if (trainOne.Position.X >= trainWidth) { 
+				trainOne.Position = new Vector2(trainTwo.Position.X - trainWidth, 0);
+			}
+			if (trainTwo.Position.X >= trainWidth) {
+				trainTwo.Position = new Vector2(trainOne.Position.X - trainWidth, 0);
+			}
+		}
 	}
 	
 	void OnStartPressed(){
@@ -23,6 +60,7 @@ public partial class MainMenu : Node2D
 
 		root.AddChild(newScene);
 		root.RemoveChild(currentScene);
+		currentScene.QueueFree();
 	}
 	
 	void OnCreditsPressed(){
@@ -31,7 +69,8 @@ public partial class MainMenu : Node2D
 
 		root.AddChild(newScene);
 		root.RemoveChild(currentScene);
-	}
+        currentScene.QueueFree();
+    }
 	
 	void OnQuitPressed(){
 		GetTree().Quit();
